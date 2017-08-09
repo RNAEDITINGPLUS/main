6c306c3d3109b10a7d84cf88a5ace7fe6f9dd9f7
Li Yao <yaoli95@outlook.com>
2017-08-09 20:04:50 +0800

    node deployment

--- node/deploy.sh
+++ node/deploy.sh
@@ -1,33 +1,60 @@
 #!/usr/bin/env bash
 HOMEDIR=$(cd `dirname $0`; pwd)
 
-echo "Making a dir to save reference files.\n"
-mkdir ref
-cd ref
-echo "Initiating download.\n"
-echo "============================================"
+echo "============================================\n"
+echo "Enter database host:\n"
+echo "============================================\n"
+read dbh
+sed -i "s?{DBH}?$dbh?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
+echo "Enter database user:\n"
+echo "============================================\n"
+read dbu
+sed -i "s?{DBU}?$dbu?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
+echo "Enter database password:\n"
+echo "============================================\n"
+read dbpw
+sed -i "s?{DBPW}?$dbpw?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
+echo "Enter database name:\n"
+echo "============================================\n"
+read dbn
+sed -i "s?{DBN}?$dbn?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
+echo "Enter database port:\n"
+echo "============================================\n"
+read dbpt
+sed -i "s?{DBPT}?$dbpt?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
 echo "Downloading reference files (hg38).\n"
-echo "============================================"
+echo "============================================\n"
 wget https://www.rnaeditplus.org/Public/ref/reference.tar.gz
-echo "============================================"
+echo "============================================\n"
 echo "Decomposing\n"
-echo "============================================"
+echo "============================================\n"
 tar -zxvf reference.tar.gz
 sed -i "s?{REFPATH}?$HOMEDIR/ref?g" $HOMEDIR/node/config.conf
-cd ..
-echo "============================================"
+sed -i "s?{RUNPATH}?$HOMEDIR?g" $HOMEDIR/node/config.conf
+echo "============================================\n"
 echo "Installing system packages.\n"
-echo "============================================"
+echo "============================================\n"
 sudo apt-get install libmysqld-dev
 sudo apt-get install python-dev
-echo "============================================"
+echo "============================================\n"
 echo "Installing python packages.\n"
-echo "============================================"
+echo "============================================\n"
 pip install -r prerequisite.txt
 pip install fisher
-tar -zxvf REDItools-1.0.4.tar.gz
 cd reditools-1.0.4
 sudo python setup.py install
-wget http://mirmap.ezlab.org/downloads/miRmap-1.1.tar.gz
-tar -zxvf miRmap-1.1.tar.gz
-cd miRmap-1.1
+echo "============================================\n"
+echo "Compiling libsvm packages.\n"
+echo "============================================\n"
+cd libsvm-3.22/python
+make
+cd ..
+mv libsvm.so.2 ..
+echo "============================================\n"
+echo "Successfully configured the computing node\n"
+echo "============================================\n"
